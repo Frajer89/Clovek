b50ef7a0003c9f02b12da9e3f3834252788cb551
Ondřej Kryštof <Krystof.Ondrej@stud.vassboskovice.cz>
2023-10-23 16:25:50 +0200

    Přidejte soubory projektu.

---
+++ Clovek/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clovek
+{
+    public partial class MainWindow : Window
+    {
+        private Person pepa;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            pepa = new Person("Pepa", 0);
+            UpdateTextBlocks();
+        }
+
+        private void Starnout_Click(object sender, RoutedEventArgs e)
+        {
+            if (pepa.Vek >= 120)
+            {
+                MessageBox.Show("Pepa je nejstarší člověk na světě s 120 lety!", "Gratulace");
+                Close();
+            }
+            else
+            {
+                pepa.Starnout(5); // Zestárni Pepu o 5 let
+                UpdateTextBlocks();
+            }
+        }
+
+        private void UpdateTextBlocks()
+        {
+            jmenoTextBlock.Text = pepa.Jmeno;
+            vekTextBlock.Text = $"{pepa.Jmeno} má {pepa.Vek} roků";
+        }
+    }
+}
---
+++ Clovek/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+public class Person
+{
+    public string Jmeno { get; private set; }
+    public int Vek { get; private set; }
+
+    public Person(string jmeno, int vek)
+    {
+        Jmeno = jmeno;
+        Vek = vek;
+    }
+
+    public void Starnout(int roky)
+    {
+        Vek += roky;
+    }
+}
